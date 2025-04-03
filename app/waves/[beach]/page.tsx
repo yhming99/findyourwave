0d9962f509be24e95ba28164ed71a1bc42d880f0
yhming99 <yhming99@gmail.com>
2025-04-03 14:10:58 +0900

    비치 페이지 컴포넌트 수정: BeachPage를 비동기 함수로 변경하여 데이터 fetching 로직을 지원하도록 개선.

--- app/waves/[beach]/page.tsx
+++ app/waves/[beach]/page.tsx
@@ -1,6 +1,5 @@
 import BeachPageClient from './beach-page-client';
 
-
-export default function BeachPage({ params }: { params: { beach: string } }) {
+export default async function BeachPage({ params }: { params: { beach: string } }) {
   return <BeachPageClient beach={params.beach} />;
 } 
